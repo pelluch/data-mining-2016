113674ba20d9fa518d933fbe653c775a8a9527c7
Pablo Lluch <pablo.lluch@email.com>
2016-08-30 12:22:39 -0300

    Tarea 1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# data-mining-2016
+El programa está probado utilizando python2. No es compatible con python3.
+
+# Uso:
+python2 main.py ARCHIVO_INPUT ARCHIVO_OUTPUT
+
+# Notas: 
+python2 es el ejecutable de python, reemplazar por el corresponda.
+ARCHIVO_INPUT es obligatorio y contiene los datos de input.
+ARCHIVO_OUTPUT es opcional, pero si se entrega contendrá el resultado del ordenamiento especificado.
+El programa mismo pide la confianza y soporte mínimo, así como el separador.
+El programa preprocesa el archivo de input eliminando espacios extras entre items.
--- main.py
+++ main.py
@@ -1,77 +1,119 @@
 #!/bin/env python2
+# -*- coding: utf-8 -*-
 
 import sys
 import csv
 from collections import defaultdict
 from itertools import chain, combinations
-import itertools
+import os
 
 
-separator = ','
-min_support = 0.4
-min_confidence = 0.1
 items = set()
 transactions = []
 k_frequencies = defaultdict(int)
 
-def joinSet(itemSet, length):
+# Función que expande las canastas de k -> k + 1 items
+def expand_set(itemSet, length):
 	return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
 
-def subsets(arr):
-	return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
+# Genera subconjuntos a partir de un arreglo
+# Ejemplo tipo http://stackoverflow.com/questions/374626/how-can-i-find-all-the-subsets-of-a-set-with-exactly-n-elements
+def subsets(item_arr):
+	return chain(*[combinations(item_arr, i + 1) for i, a in enumerate(item_arr)])
 
 def process_args(args):
+	if len(args) < 2:
+		print('Debes ingresar un archivo como parámetro')
+		return
 	file = args[1]
+	
+	if not os.path.isfile(file):
+		print('Archivo indicado no existe')
+		return
+	
+
+	min_confidence = -1
+	min_support = -1
+
+	while min_confidence < 0 or min_confidence > 1:
+		min_confidence = float(raw_input('Ingrese 0 <= min_confidence <= 1\n'))
+
+	while min_support < 0 or min_support > 1:
+		min_support = float(raw_input('Ingrese 0 <= min_support <= 1\n'))
+
+	# Cargamos los datos
 	items, transactions = process_file(file)
-	k_items = prune_baskets(items)
+	# Items "pruneados"
+	k_items = prune_baskets(items, min_support)
 	k = 2
-	all_sets = dict()
+	# Esto contendrá todos los items frecuentes para cada k
+	all_sets = dict()	
 
 	while(k_items != set([])):
 		all_sets[k-1] = k_items
-		k_items = joinSet(k_items, k)
-		k_items = prune_baskets(k_items)
+		k_items = expand_set(k_items, k)
+		k_items = prune_baskets(k_items, min_support)
 		k = k + 1
 
-	toRetItems = []
-	for k, rules in all_sets.items():	    
-	
-		toRetItems.extend([(tuple(item), getSupport(item))
-                           for item in rules])
-
-	toRetRules = []
+	final_rules = []
 	for k, rules in all_sets.items()[1:]:
 		for rule in rules:
-			rule_subsets = map(frozenset, [x for x in subsets(item)])
+			# Para cada regla, encontramos sus subconjuntos
+			rule_subsets = map(frozenset, [x for x in subsets(rule)])
 			for rule_subset in rule_subsets:
+				# Obtenemos lo que eudó afuera
 				remain = rule.difference(rule_subset)
 				if len(remain) > 0:
-					support = getSupport(rule)
-					confidence = support/getSupport(rule_subset)
-					lift = confidence/(getSupport(remain))
+					support = calculate_support(rule)
+					confidence = support/calculate_support(rule_subset)
+					lift = confidence/(calculate_support(remain))
 					if confidence >= min_confidence:
-						toRetRules.append(((','.join(rule_subset) + ';' + ','.join(remain)),
+						# Generamos tupla con información que permite ordenar después
+						final_rules.append(((','.join(sorted(rule_subset)) + ';' + ','.join(sorted(remain))),
                                        support, confidence, lift))
+	order = "1"			
+	output_file = None			
+	while order != "4":	
+		order = raw_input("1) Ordenar por soporte\n2) Ordenar por confidence\n3) Ordenar por lift\n4) Salir\n")
+		if order == "1":
+			sorted_rules = sorted(final_rules, key=lambda tup: tup[1], reverse=True)
+		elif order == "2":	
+			sorted_rules = sorted(final_rules, key=lambda tup: tup[2], reverse=True)
+		elif order == "3":
+			sorted_rules = sorted(final_rules, key=lambda tup: tup[3], reverse=True)
+		elif order == "4":
+			return
 
-	sorted_rules = sorted(toRetRules, key=lambda tup: tup[1])
-	
+		if len(args) > 2:
+			output = args[2]
+			output_file = open(output, 'w')
+		for rule in sorted_rules:
+			print(';'.join(map(str, [x for x in rule])))
+			if output_file is not None:
+				output_file.write(';'.join(map(str, [x for x in rule])) + "\n")
+		if output_file is not None:
+			output_file.close()
 
 
+# Al usar intern, se hace más eficiente el uso de strings sin necesidad de usar enteros
 def interned_item(item):
 	return intern(item.strip())
 
-def getSupport(item):
+# Calcula soporte para un conjunto
+def calculate_support(item):
 	return float(k_frequencies[item])/len(transactions)
 
-def prune_baskets(current_items):
+def prune_baskets(current_items, min_support):
 
 	new_items = set()
+	# Frecuencias para set actual
 	k_set = defaultdict(int)
 
 	for item in current_items:
 		for transaction in transactions:
 			if item.issubset(transaction):
 				k_set[item] += 1
+				# Set global para calcular confidence a posteriori
 				k_frequencies[item] += 1
 	
 	for item, count in k_set.items():
@@ -82,12 +124,17 @@ def prune_baskets(current_items):
 				
 
 def process_file(path):
+	separator = ''	
+	while len(separator) != 1:
+		separator = raw_input('Ingrese el separador para el input (un caracter)\n')
 	with open(path, 'r') as csvfile:
 		input_data = csv.reader(csvfile, delimiter=separator, quotechar='"')
 		for row in input_data:
+			# Optimización para mejorar comparaciones de strings
 			row_items = list(map(interned_item, row))
 			transactions.append(row_items)
 			for item in row_items:
+				# frozenset permite manejar conjuntos de modo fácil
 				items.add(frozenset([item]))
 		return items, transactions
 
